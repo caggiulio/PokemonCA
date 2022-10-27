9d109cf923979c81b5b2f08c55f9f829c628ac28
Nunzio Giulio Caggegi <nunziogiuliocaggegi@gmail.com>
2022-10-27 16:16:01 +0200

    adds open pokemon intent

---
+++ PokemonTest/AppIntents/Action/OpenPokemon.swift
@@ -0,0 +1,28 @@
+//
+//  OpenPokemon.swift
+//  PokemonTest
+//
+//  Created by Nunzio Giulio Caggegi on 27/10/22.
+//
+
+import AppIntents
+import SwiftUI
+
+/// This is the real implementation of `AppIntent`. Is the action to show the information about a `PokemonAppEntity`.
+@available(iOS 16, *)
+struct OpenPokemon: AppIntent {
+  /// The title of the action in the `Shortcut` app.
+  static var title: LocalizedStringResource = LocalizedStringResource(stringLiteral: PKMNString.AppIntents.title)
+  
+  /// The description of the action.
+  static var description: IntentDescription = IntentDescription(stringLiteral: PKMNString.AppIntents.description)
+  
+  /// The dynamic lookup parameter.
+  @Parameter(title: LocalizedStringResource(stringLiteral: PKMNString.AppIntents.pokemonParamaterDescription), requestValueDialog: IntentDialog(stringLiteral: PKMNString.AppIntents.dialog))
+  var pokemon: PokemonAppEntity
+  
+  @MainActor
+  func perform() async throws -> some ReturnsValue<PokemonAppEntity> {
+    return .result(value: pokemon, view: PokemonInformation(pokemon: pokemon))
+  }
+}
--- PokemonTest/PKMNCore/Infrastructure/Assets/PKMNString.swift
+++ PokemonTest/PKMNCore/Infrastructure/Assets/PKMNString.swift
@@ -9,11 +9,20 @@ import Foundation
 
 // MARK: - PKMNString
 
-public enum PKMNString {
+public struct PKMNString {
   public static let standardError = PKMNString.tr("Localizable", "standard_error")
   public static let standardSearch = PKMNString.tr("Localizable", "standard_search")
   public static let resourceNotFound = PKMNString.tr("Localizable", "resource_not_found")
   public static let homeTitle = PKMNString.tr("Localizable", "home_title")
+}
+
+extension PKMNString {
+  public struct AppIntents {
+    public static let title = PKMNString.tr("Localizable", "appIntents.open.title")
+    public static let description = PKMNString.tr("Localizable", "appIntents.open.description")
+    public static let pokemonParamaterDescription = PKMNString.tr("Localizable", "appIntents.open.pokemonParameterDescription")
+    public static let dialog = PKMNString.tr("Localizable", "appIntents.open.dialog")
+  }
 }
 
 extension PKMNString {

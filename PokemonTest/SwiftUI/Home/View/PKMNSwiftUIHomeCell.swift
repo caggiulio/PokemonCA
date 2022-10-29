70f761fb4535ceb20f88e7ce8d1cd145e1e430e2
Nunzio Giulio Caggegi <nunziogiuliocaggegi@gmail.com>
2022-10-29 10:59:15 +0200

    adds background for view

--- PokemonTest/SwiftUI/Home/View/PKMNSwiftUIHomeCell.swift
+++ PokemonTest/SwiftUI/Home/View/PKMNSwiftUIHomeCell.swift
@@ -47,7 +47,25 @@ struct PKMNHomeCell: View {
       
       Spacer()
     }
-    .background(Color(uiColor: AppAsset.primary.color))
+    .background {
+      PKMNHomeCellBackground()
+    }
     .clipShape(RoundedRectangle(cornerRadius: 20))
+  }
+}
+
+/// The background `View` for the view.
+struct PKMNHomeCellBackground: View {
+  var body: some View {
+    ZStack {
+      LinearGradient(
+        colors:
+          [
+            Color(uiColor: AppAsset.primary.color),
+            Color(uiColor: AppAsset.primary.color.withAlphaComponent(0.35))
+          ],
+        startPoint: .top,
+        endPoint: .bottom)
+    }
   }
 }
